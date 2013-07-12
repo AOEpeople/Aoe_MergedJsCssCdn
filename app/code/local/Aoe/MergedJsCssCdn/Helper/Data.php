f3e2c2581cb6516f3e0d5ab64f2d3f21978a2e04
Dmytro Zavalkin <dmytro.zavalkin@aoemedia.de>
2013-07-12 15:31:56 +0200

    MAGE-437: Prepare for Cloud: Make CSS and JS ouside of media - implemented cdn support for merged css/js files (upload/url substitution)

---
+++ app/code/local/Aoe/MergedJsCssCdn/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author Dmytro Zavalkin <dmytro.zavalkin@aoemedia.de>
+ */
+
+class Aoe_MergedJsCssCdn_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
---
+++ app/code/local/Aoe/MergedJsCssCdn/Model/Package.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @author Dmytro Zavalkin <dmytro.zavalkin@aoemedia.de>
+ */
+class Aoe_MergedJsCssCdn_Model_Package extends Aoe_JsCssTstamp_Model_Package
+{
+    /**
+     * Generate CDN url for merged file of given $type.
+     * Upload file to CDN if it doesn't exist there yet
+     *
+     * @param string $type
+     * @param array $files
+     * @param string $targetDir
+     * @param string $targetFilename
+     * @return string
+     */
+    protected function generateMergedUrl($type, array $files, $targetDir, $targetFilename)
+    {
+        $nativeUrl = parent::generateMergedUrl($type, $files, $targetDir, $targetFilename);
+
+        /* @var $helper Aoe_Cdn_Helper_Data */
+        $helper = Mage::helper('aoecdn');
+        $path = $targetDir . DS . $targetFilename;
+        // relative path
+        $relativePath = ltrim(str_replace(Mage::getBaseDir('media'), '', $path), DS);
+
+        $url = $helper->getCdnUrl($relativePath);
+        if (!$url && is_file($path)) {
+            $url = $helper->storeInCdn($path);
+        }
+
+        if ($url) {
+            Mage::log(sprintf('Copied merged %s file "%s" to cdn. Url "%s"', $type, $path, $url), Zend_Log::DEBUG);
+        } else {
+            $url = $nativeUrl;
+            Mage::log(sprintf('Did not copy merged %s file "%s" to cdn.', $type, $path), Zend_Log::ERR);
+        }
+
+        return $url;
+    }
+}
